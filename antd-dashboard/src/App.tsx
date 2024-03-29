9f29ba9dd7c5f49020701ca78967f5cf2ecf2e81
madeha.youssof <madeha.youssof@itland-sy.com>
2024-03-29 06:54:59 -0400

    create app structure

--- antd-dashboard/src/App.tsx
+++ antd-dashboard/src/App.tsx
@@ -1,7 +1,21 @@
 import React from "react";
+import Header from "./layouts/header";
+import Sidebar from "./layouts/sidebar";
+import Content from "./layouts/content";
+import Footer from "./layouts/Footer";
+import { Space } from "antd";
 
 function App() {
-  return <div className="App">App</div>;
+  return (
+    <div className="app">
+      <Header />
+      <Space>
+        <Sidebar></Sidebar>
+        <Content></Content>
+      </Space>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
